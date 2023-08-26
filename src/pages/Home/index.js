a528c1ce0a655b301c653d8789b6b3318f050181
chessoft <chessoft@hotmail.com>
2023-08-27 00:10:15 +0200

    correction ancres menu

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -24,7 +24,7 @@ const Page = () => {
         <section className="SliderContainer">
           <Slider />
         </section>
-        <section className="ServicesContainer">
+        <section className="ServicesContainer" id="nos-services">
           <h2 className="Title">Nos services</h2>
           <p>Nous organisons des événements sur mesure partout dans le monde</p>
           <div className="ListContainer">
@@ -53,11 +53,11 @@ const Page = () => {
             </ServiceCard>
           </div>
         </section>
-        <section className="EventsContainer">
+        <section className="EventsContainer" id="nos-realisations">
           <h2 className="Title">Nos réalisations</h2>
           <EventList />
         </section>
-        <section className="PeoplesContainer">
+        <section className="PeoplesContainer" id="notre-equipe">
           <h2 className="Title">Notre équipe</h2>
           <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
           <div className="ListContainer">
@@ -117,16 +117,16 @@ const Page = () => {
           <h3>Notre derniére prestation</h3>
           {last && ( // last n'existait pas dans le context donc on ne cree que si last !=undefined
             <Modal key={last.id} Content={<ModalEvent event={last} />}>
-                  {({ setIsOpened }) => (
-                    <EventCard
-                      onClick={() => setIsOpened(true)}
-                      imageSrc={last.cover}
-                      title={last.title}
-                      date={new Date(last.date)}
-                      small // sans valeur = true
-                      label={last.type}
-                    />
-                  )}
+              {({ setIsOpened }) => (
+                <EventCard
+                  onClick={() => setIsOpened(true)}
+                  imageSrc={last.cover}
+                  title={last.title}
+                  date={new Date(last.date)}
+                  small // sans valeur = true
+                  label={last.type}
+                />
+              )}
             </Modal>
           )}
         </div>
